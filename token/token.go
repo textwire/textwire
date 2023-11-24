80e061ccadceb98fbf2b994b7d8db00dea08fc2a
SerhiiCho <serhiicho@protonmail.com>
2023-11-24 10:52:09 +0200

    Add token types to a token.go

--- token/token.go
+++ token/token.go
@@ -1,8 +1,27 @@
 package token
 
-type TokenType string
+type TokenType int
+
+const (
+	// Special types
+	ILLEGAL TokenType = iota // An illegal token
+	EOF                      // The end of the file
+
+	// Identifiers + literals
+	IDENT // foo, bar
+	INT   // 4, 24
+
+	// Delimiters
+	OPEN_BRACES  // {{
+	CLOSE_BRACES // }}
+
+	// Keywords
+	IF
+)
 
 type Token struct {
 	Type    TokenType
 	Literal string
+	Line    uint
+	File    string
 }
