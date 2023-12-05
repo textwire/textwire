b11109b98b37f9ee8532e248dba95ee558927ae7
SerhiiCho <serhiicho@protonmail.com>
2023-12-05 21:25:00 +0200

    WIP on parser implementation

---
+++ ast/program.go
@@ -0,0 +1,25 @@
+package ast
+
+import "bytes"
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var result bytes.Buffer
+
+	for _, stmt := range p.Statements {
+		result.WriteString(stmt.String())
+	}
+
+	return result.String()
+}
---
+++ parser/parser.go
@@ -0,0 +1,26 @@
+package parser
+
+import (
+	"go/token"
+
+	"github.com/textwire/textwire/ast"
+	"github.com/textwire/textwire/lexer"
+)
+
+type Parser struct {
+	lexer  *lexer.Lexer
+	errors []string
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(lexer *lexer.Lexer) *Parser {
+	p := &Parser{lexer: lexer}
+
+	return p
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	// todo: here
+}
