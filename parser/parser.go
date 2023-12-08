ac42b42e65380ab4f778d269c41912e680394fbc
SerhiiCho <serhiicho@protonmail.com>
2023-12-08 19:32:22 +0200

    WIP on parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -1,30 +1,113 @@
 package parser
 
 import (
-	"go/token"
+	"github.com/textwire/textwire/token"
 
 	"github.com/textwire/textwire/ast"
 	"github.com/textwire/textwire/lexer"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
-	lexer  *lexer.Lexer
+	l      *lexer.Lexer
 	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(lexer *lexer.Lexer) *Parser {
-	p := &Parser{lexer: lexer}
+	p := &Parser{
+		l:      lexer,
+		errors: []string{},
+	}
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
 
 	return p
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	// todo: here
+	prog := &ast.Program{}
+	prog.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			prog.Statements = append(prog.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return prog
 }
 
 func (p *Parser) Errors() []string {
 	return p.errors
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.HTML:
+		return p.parseHTMLStatement()
+	case token.LBRACES:
+		return p.parseEmbeddedCode()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) curTokenIs(tok token.TokenType) bool {
+	return p.curToken.Type == tok
+}
+
+func (p *Parser) peekTokenIs(tok token.TokenType) bool {
+	return p.peekToken.Type == tok
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+}
+
+func (p *Parser) parseHTMLStatement() *ast.HTMLStatement {
+	return &ast.HTMLStatement{Token: p.curToken}
+}
+
+func (p *Parser) parseEmbeddedCode() ast.Statement {
+	p.nextToken()
+
+	return p.parseExpressionStatement()
+}
+
+func (p *Parser) parseExpressionStatement() ast.Statement {
+	expr := p.parseExpression(LOWEST)
+	result := &ast.ExpressionStatement{Token: p.curToken, Expression: expr}
+
+	if p.peekTokenIs(token.RBRACES) {
+		p.nextToken()
+	}
+
+	return result
 }
