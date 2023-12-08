2422c0bc026ce2a3852db7d58b98171c55f2a29b
SerhiiCho <serhiicho@protonmail.com>
2023-12-08 19:32:04 +0200

    Add expression statement ast node

---
+++ ast/expression_statement.go
@@ -0,0 +1,23 @@
+package ast
+
+import "github.com/textwire/textwire/token"
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (e *ExpressionStatement) statementNode() {
+}
+
+func (e *ExpressionStatement) TokenLiteral() string {
+	return e.Token.Literal
+}
+
+func (e *ExpressionStatement) String() string {
+	if e.Expression == nil {
+		return ""
+	}
+
+	return e.Expression.String()
+}
