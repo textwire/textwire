e15421b240253d2fdc32b061d6d691c8506d69df
SerhiiCho <serhiicho@protonmail.com>
2023-12-10 08:32:18 +0200

    Add more to testing identifier

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"testing"
 
+	"github.com/textwire/textwire/ast"
 	"github.com/textwire/textwire/lexer"
 )
 
@@ -19,6 +20,37 @@ func TestParseIdentifier(t *testing.T) {
 	if len(program.Statements) != 1 {
 		t.Fatalf("program must have 1 statement, got %d", len(program.Statements))
 	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an ExpressionStatement, got %T", program.Statements[0])
+	}
+
+	if !checkIdentifier(t, stmt.Expression, "myName") {
+		return
+	}
+}
+
+func checkIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+
+	if !ok {
+		t.Errorf("exp is not an Identifier, got %T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value is not %s, got %s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral() is not %s, got %s", value, ident.TokenLiteral())
+		return false
+	}
+
+	return true
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
