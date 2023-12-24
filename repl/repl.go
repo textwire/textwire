ca68ecbaf7bda8cd84927f9351eb4a2f99730ead
SerhiiCho <serhiicho@protonmail.com>
2023-12-24 18:59:13 +0200

    Add repl.go

---
+++ repl/repl.go
@@ -0,0 +1,55 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/textwire/textwire/evaluator"
+	"github.com/textwire/textwire/lexer"
+	"github.com/textwire/textwire/object"
+	"github.com/textwire/textwire/parser"
+)
+
+const PROMPT = "$ "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnv()
+
+	for {
+		fmt.Print(PROMPT)
+
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		lex := lexer.New(line)
+		pars := parser.New(lex)
+
+		program := pars.ParseProgram()
+
+		if len(pars.Errors()) != 0 {
+			printParserErrors(out, pars.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program, env)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
