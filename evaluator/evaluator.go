acc42ed62ec7ffe7194e03e460e0d43dec5dbf9e
SerhiiCho <serhiicho@protonmail.com>
2023-12-25 20:58:00 +0200

    Add test for strings to evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,6 +15,10 @@ func Eval(node ast.Node, env *object.Env) object.Object {
 	switch node := node.(type) {
 
 	// Statements
+	case *ast.Program:
+		return evalProgram(node, env)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression, env)
 
 	// Expressions
 	case *ast.Identifier:
@@ -26,6 +30,21 @@ func Eval(node ast.Node, env *object.Env) object.Object {
 	}
 
 	return nil
+}
+
+func evalProgram(program *ast.Program, env *object.Env) object.Object {
+	var result object.Object
+
+	for _, statement := range program.Statements {
+		result = Eval(statement, env)
+
+		switch result := result.(type) {
+		case *object.Error:
+			return result
+		}
+	}
+
+	return result
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Env) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -23,6 +23,21 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func TestEvalStringLiteral(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`{{ "Hello World" }}`, "Hello World"},
+		{`{{ "She is pretty" }}`, "She is pretty"},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testStringObject(t, evaluated, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -41,5 +56,17 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) {
 
 	if result.Value != expected {
 		t.Errorf("result.Value is not %d, got %d", expected, result.Value)
+	}
+}
+
+func testStringObject(t *testing.T, obj object.Object, expected string) {
+	result, ok := obj.(*object.String)
+
+	if !ok {
+		t.Errorf("obj is not a String, got %T", obj)
+	}
+
+	if result.Value != expected {
+		t.Errorf("result.Value is not %s, got %s", expected, result.Value)
 	}
 }
