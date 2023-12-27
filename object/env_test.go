0f37afa2a475075bc9a118d3b5add1818a82939a
SerhiiCho <serhiicho@protonmail.com>
2023-12-27 13:11:05 +0200

    Change test for EnvFromMap function

--- object/env_test.go
+++ object/env_test.go
@@ -3,65 +3,61 @@ package object
 import "testing"
 
 func TestEnvFromMap(t *testing.T) {
-	t.Run("with string values", func(t *testing.T) {
-		m := map[string]interface{}{
-			"foo": "bar",
-			"baz": "qux",
+	vars := map[string]interface{}{
+		"title":  "Hello, World!",
+		"n":      -1,
+		"num":    int8(-2),
+		"num1":   int16(-3),
+		"num2":   int32(-4),
+		"num3":   int64(-5),
+		"un":     uint(1),
+		"unum":   uint8(2),
+		"unum1":  uint16(3),
+		"unum2":  uint32(4),
+		"unum3":  uint64(5),
+		"bool1":  true,
+		"bool2":  false,
+		"height": 5.7,
+		"weight": float32(62.2),
+	}
+
+	expect := map[string]Object{
+		"title":  &String{Value: "Hello, World!"},
+		"n":      &Int{Value: -1},
+		"num":    &Int8{Value: -2},
+		"num1":   &Int16{Value: -3},
+		"num2":   &Int32{Value: -4},
+		"num3":   &Int64{Value: -5},
+		"un":     &Uint{Value: 1},
+		"unum":   &Uint8{Value: 2},
+		"unum1":  &Uint16{Value: 3},
+		"unum2":  &Uint32{Value: 4},
+		"unum3":  &Uint64{Value: 5},
+		"bool1":  &Boolean{Value: true},
+		"bool2":  &Boolean{Value: false},
+		"height": &Float64{Value: 5.7},
+		"weight": &Float32{Value: 62.2},
+	}
+
+	env, err := EnvFromMap(vars)
+
+	if err != nil {
+		t.Fatalf("EnvFromMap returned an error: %s", err)
+	}
+
+	for key, val := range expect {
+		obj, ok := env.Get(key)
+
+		if !ok {
+			t.Fatalf("Env.Get(%s) returned !ok", key)
 		}
 
-		env, err := EnvFromMap(m)
-
-		if err != nil {
-			t.Fatalf("EnvFromMap returned an error: %s", err)
+		if obj.Type() != val.Type() {
+			t.Fatalf("Env.Get(%s) returned %s, expected %s", key, obj.Type(), val.Type())
 		}
 
-		for key, val := range m {
-			obj, ok := env.Get(key)
-
-			if !ok {
-				t.Fatalf("Env.Get returned false for key %s", key)
-			}
-
-			str, ok := obj.(*String)
-
-			if !ok {
-				t.Fatalf("Env.Get returned a non-String object for key %s", key)
-			}
-
-			if str.Value != val.(string) {
-				t.Fatalf("Env.Get returned a String object with an incorrect value for key %s", key)
-			}
+		if obj.String() != val.String() {
+			t.Fatalf("Env.Get(%s) returned %s, expected %s", key, obj.String(), val.String())
 		}
-	})
-
-	t.Run("with int values", func(t *testing.T) {
-		m := map[string]interface{}{
-			"foo": 1,
-			"baz": 2,
-		}
-
-		env, err := EnvFromMap(m)
-
-		if err != nil {
-			t.Fatalf("EnvFromMap returned an error: %s", err)
-		}
-
-		for key, val := range m {
-			obj, ok := env.Get(key)
-
-			if !ok {
-				t.Fatalf("Env.Get returned false for key %s", key)
-			}
-
-			integer, ok := obj.(*Int)
-
-			if !ok {
-				t.Fatalf("Env.Get returned a non-Integer object for key %s", key)
-			}
-
-			if integer.Value != val {
-				t.Fatalf("Env.Get returned an Integer object with an incorrect value for key %s", key)
-			}
-		}
-	})
+	}
 }
