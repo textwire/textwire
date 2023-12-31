c5a836df7e134b6b22314dfa0bdf65a4dba6c059
SerhiiCho <serhiicho@protonmail.com>
2023-12-31 16:15:30 +0200

    Add InfixExpression struct to ast package

---
+++ ast/infix_expression.go
@@ -0,0 +1,24 @@
+package ast
+
+import (
+	"fmt"
+
+	"github.com/textwire/textwire/token"
+)
+
+type InfixExpression struct {
+	Token    token.Token // The operator token, e.g. +
+	Operator string      // The operator, e.g. +
+	Left     Expression
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode() {}
+
+func (ie *InfixExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *InfixExpression) String() string {
+	return fmt.Sprintf("(%s %s %s)", ie.Left, ie.Operator, ie.Right)
+}
