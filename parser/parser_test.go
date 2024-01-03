5aaf67cc1511b0b5cd0e6650dda9c49c6be6daa2
SerhiiCho <serhiicho@protonmail.com>
2024-01-03 16:39:48 +0200

    Add test cases for operator precedence parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -338,3 +338,42 @@ func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
 
 	return true
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			"{{ 1 * 2 }}",
+			"{{ (1 * 2) }}",
+		},
+		{
+			"<h2>{{ -2 + 3 }}</h2>",
+			"<h2>{{ ((-2) + 3) }}</h2>",
+		},
+		{
+			"{{ a + b + c }}",
+			"{{ ((a + b) + c) }}",
+		},
+		{
+			"{{ a + b / c }}",
+			"{{ (a + (b / c)) }}",
+		},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+
+		program := p.ParseProgram()
+
+		checkParserErrors(t, p)
+
+		actual := program.String()
+
+		if actual != tt.expected {
+			t.Errorf("expected=%q, got=%q", tt.expected, actual)
+		}
+	}
+}
