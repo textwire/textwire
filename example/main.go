94fa092c7227448ced6ec3cb1c639a136e0d3cdb
SerhiiCho <serhiicho@protonmail.com>
2024-01-17 20:35:56 +0200

    Change example files

--- example/main.go
+++ example/main.go
@@ -14,13 +14,36 @@ func main() {
 	})
 
 	http.HandleFunc("/", homeHandler())
+	http.HandleFunc("/about", aboutHandler())
 
 	fmt.Println("Listening on http://localhost:8080")
+
 	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
 
 func homeHandler() http.HandlerFunc {
 	template, err := textwire.ParseTemplate("home")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := map[string]interface{}{
+			"title": "Hello, World!",
+			"age":   23,
+		}
+
+		err := template.EvaluateResponse(w, vars)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func aboutHandler() http.HandlerFunc {
+	template, err := textwire.ParseTemplate("about")
 
 	if err != nil {
 		fmt.Println(err)
