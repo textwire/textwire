4f8389b9eb1a23c073d06fb25a45eca66c39a084
SerhiiCho <serhiicho@protonmail.com>
2024-01-23 21:54:07 +0200

    Add post_expression.go ast node

---
+++ ast/post_expression.go
@@ -0,0 +1,24 @@
+package ast
+
+import (
+	"fmt"
+
+	"github.com/textwire/textwire/token"
+)
+
+type PostfixExpression struct {
+	Token    token.Token // The '++' or '--' token
+	Operator string
+	Left     Expression
+}
+
+func (pe *PostfixExpression) expressionNode() {
+}
+
+func (pe *PostfixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PostfixExpression) String() string {
+	return fmt.Sprintf("(%s%s)", pe.Left, pe.Operator)
+}
