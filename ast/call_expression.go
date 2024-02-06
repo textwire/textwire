6dc37302c40f10fe03379690e0e9a33069b140bc
SerhiiCho <serhiicho@protonmail.com>
2024-02-06 12:32:35 +0200

    Add call expression ast node

---
+++ ast/call_expression.go
@@ -0,0 +1,41 @@
+package ast
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/textwire/textwire/token"
+)
+
+type CallExpression struct {
+	Token     token.Token // The receiver token
+	Receiver  Expression  // The receiver of the call
+	Function  Expression  // The function being called
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {
+}
+
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+
+func (ce *CallExpression) String() string {
+	var args bytes.Buffer
+
+	for i, arg := range ce.Arguments {
+		args.WriteString(arg.String())
+
+		if i < len(ce.Arguments)-1 {
+			args.WriteString(", ")
+		}
+	}
+
+	return fmt.Sprintf("%s.%s(%s)", ce.Receiver.String(),
+		ce.Function.String(), args.String())
+}
+
+func (ce *CallExpression) Line() uint {
+	return ce.Token.Line
+}
