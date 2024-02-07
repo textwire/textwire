504ac8dbe2766a0afbe56b13a094f5402f20870c
SerhiiCho <serhiicho@protonmail.com>
2024-02-07 12:26:09 +0200

    Add comment to a strLenFunc function

--- evaluator/str_functions.go
+++ evaluator/str_functions.go
@@ -2,6 +2,7 @@ package evaluator
 
 import "github.com/textwire/textwire/object"
 
+// strLenFunc returns the length of the given string
 func strLenFunc(receiver object.Object, args ...object.Object) object.Object {
 	str := receiver.(*object.Str)
 	val := len(str.Value)
