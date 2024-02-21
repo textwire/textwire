639d7ae93069138dfcea0f220264c244c7890e22
SerhiiCho <serhiicho@protonmail.com>
2024-02-21 10:22:57 +0200

    test: add test for testing object struct

--- textwire_test.go
+++ textwire_test.go
@@ -35,6 +35,10 @@ func TestEvaluateString(t *testing.T) {
 	}{
 		{"{{ 1 + 2 }}", "3", nil},
 		{"{{ n1 * n2 }}", "2", map[string]interface{}{"n1": 1, "n2": 2}},
+		{"{{ user.Name.First }}", "Ann", map[string]interface{}{"user": struct {
+			Name struct{ First string }
+			Age  int
+		}{Name: struct{ First string }{"Ann"}, Age: 20}}},
 	}
 
 	for _, tt := range tests {
