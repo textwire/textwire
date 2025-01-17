f146dc7b8e12a34947414a4f05ccdc2b39eb115a
SerhiiCho <serhiicho@protonmail.com>
2025-01-17 12:47:14 +0200

    Implemented Dump method for error object

--- object/error.go
+++ object/error.go
@@ -1,6 +1,11 @@
 package object
 
-import "github.com/textwire/textwire/v2/fail"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/textwire/textwire/v2/fail"
+)
 
 type Error struct {
 	Err *fail.Error
@@ -15,7 +20,14 @@ func (e *Error) String() string {
 }
 
 func (e *Error) Dump(ident int) string {
-	return "error object"
+	var out bytes.Buffer
+
+	out.WriteString("<span class='textwire-meta'>error >>></span>\n")
+	out.WriteString(fmt.Sprintf("<span class='textwire-key'>%s</span>\n\n", e.Err.Meta()))
+	out.WriteString(fmt.Sprintf("<span class='textwire-str'>%s</span>\n", e.Err.Message()))
+	out.WriteString("<span class='textwire-meta'><<<</span>")
+
+	return out.String()
 }
 
 func (e *Error) Val() interface{} {
