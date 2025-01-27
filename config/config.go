33272bae262462e8b31fdc12a387240a6357432b
SerhiiCho <serhiicho@protonmail.com>
2025-01-27 11:37:44 +0200

    Change comments in config.go

--- config/config.go
+++ config/config.go
@@ -3,11 +3,11 @@ package config
 // Config is the main configuration for Textwire
 type Config struct {
 	// TemplateDir is the directory where the Textwire
-	// templates are located. Default is "templates"
+	// templates are located. Default is `"templates"`
 	TemplateDir string
 
 	// TemplateExt is the extension of the Textwire
-	// template files. Default is ".tw.html"
+	// template files. Default is `.tw.html`, recommended to use`.tw`.
 	// If you use a different extension other then ".tw.html",
 	// you will loose syntax highlighting in VSCode editor
 	// if you use the Textwire extension
@@ -16,11 +16,11 @@ type Config struct {
 	// ErrorPagePath is the relative path to the custom error page
 	// that will be displayed when an error occurs while
 	// rendering a template. Default is an internal error page.
-	// It's relative to the TemplateDir directory
+	// It's relative to the `TemplateDi`r directory
 	ErrorPagePath string
 
-	// DebugMode is a flag to enable debug mode. When enabled, you
-	// can see error messages in the browser. Default is false
+	// DebugMode is a flag to enable the debug mode. When enabled,
+	// you can see error messages in the browser. Default is `false`
 	DebugMode bool
 }
 
