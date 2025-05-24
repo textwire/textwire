6df313489e8ab91eab0b00e5c9ab548bcef50582
SerhiiCho <serhiicho@protonmail.com>
2025-05-24 14:52:50 +0300

    fix: remove new line from the error message in fail package

--- fail/fail.go
+++ fail/fail.go
@@ -108,7 +108,7 @@ func (e *Error) Meta() string {
 
 // String returns the full error message with all the details
 func (e *Error) String() string {
-	return fmt.Sprintf("[%s]:\n%s", e.Meta(), e.Message())
+	return fmt.Sprintf("[%s]: %s", e.Meta(), e.Message())
 }
 
 // FatalOnError calls log.Fatal if the error message is not empty
