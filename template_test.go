d230b11668c0fd892736633da5606916c3c20e11
SerhiiCho <serhiicho@protonmail.com>
2026-01-28 18:07:01 +0200

    refactor: add underscore prefix to custom func

--- template_test.go
+++ template_test.go
@@ -174,7 +174,7 @@ func TestRegisteringCustomFunction(t *testing.T) {
 		t.Fatalf("unexpected error: %s", err)
 	}
 
-	err = RegisterStrFunc("secondLetterUppercase", func(s string, args ...any) any {
+	err = RegisterStrFunc("_secondLetterUppercase", func(s string, args ...any) any {
 		if len(s) < 2 {
 			return s
 		}
