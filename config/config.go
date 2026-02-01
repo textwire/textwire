8b3d6fd90f6bcdf80145d81b363530c0710eb291
SerhiiCho <serhiicho@protonmail.com>
2026-02-01 18:33:10 +0200

    refactor: config.go

--- config/config.go
+++ config/config.go
@@ -1,31 +1,41 @@
 package config
 
-// Config is the main configuration for Textwire
+import "io/fs"
+
+// Config holds the configuration settings for Textwire template engine.
 type Config struct {
-	// TemplateDir is the directory where the Textwire templates are
-	// located. Default is `"templates"`.
+	// TemplateDir specifies the directory containing Textwire template files.
+	// Default: "templates"
+	// Note: If TemplatesFS is provided, it will be used for templates path
+	// instead of TemplateDir.
 	TemplateDir string
 
-	// TemplateExt is the extension of the Textwire template files.
-	// Default is `.tw`. If you use a different extension other then
-	// ".tw", you will loose syntax highlighting in VSCode editor if
-	// you use the Textwire extension.
+	// TemplatesFS provides an optional fs.FS filesystem for template access.
+	// Default: os.DirFS(TemplateDir)
+	// Use this field to embed templates into your binary using Go's embed package.
+	// When provided, TemplateDir is not used for file access.
+	TemplatesFS fs.FS
+
+	// TemplateExt defines the file extension for Textwire template files.
+	// Default: ".tw"
+	// Note: Using a different extension may disable syntax highlighting
+	// in editors like VSCode when using the Textwire extension.
 	TemplateExt string
 
-	// ErrorPagePath is the relative path to the custom error page that
-	// will be displayed when an error occurs while rendering a template.
-	// Default is an internal error page. It's relative to the `TemplateDir`
-	// directory.
+	// ErrorPagePath sets the relative path to a custom error page template.
+	// Default: internal error page
+	// The path is relative to the template directory (TemplateDir or TemplatesFS root).
 	ErrorPagePath string
 
-	// DebugMode is a flag to enable the debug mode. When enabled, you
-	// can see error messages in the browser. Default is `false`.
+	// DebugMode enables detailed error reporting in the browser and server logs.
+	// Default: false (keep false in production)
+	// When true, error messages with file paths and line numbers are displayed
+	// during development.
 	DebugMode bool
 
-	// GlobalData contains data shared throughout all of you templates,
-	// including components, layouts, etc. It's good for storing
-	//  configurations like env variables. You can access shared data
-	// using global object `global`. For example: `global.authUser`
+	// GlobalData stores shared data accessible across all templates.
+	// Access these values in templates using the `global` object (e.g., `global.authUser`).
+	// Useful for storing environment variables, configuration, or common data.
 	GlobalData map[string]any
 }
 
